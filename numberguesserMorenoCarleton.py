af4471e84d39c4970f192eecb903ecd01ecedcc8
sn0w0b <167842670+sn0w0b@users.noreply.github.com>
2025-02-21 19:45:57 -0600

    Update numberguesserMorenoCarleton.py

--- numberguesserMorenoCarleton.py
+++ numberguesserMorenoCarleton.py
@@ -8,16 +8,19 @@ import random
 
 answer = random.randrange(1,11)
 
-guess = int(input("Please guess a number between 1 and 10: "))
+guess = int(input("Please guess a number between 1 and 11: "))
 
 for i in range(100):
         
-    if guess == answer:
-            print("Well done, you guessed it!")
-            break
-    elif guess > answer:
+if guess == 0:
+    print("Process finished with exit code 0")
+    break
+elif guess == answer:
+        print("Well done, you guessed it!")
+        break
+elif guess > answer:
             print("Lower")
-    else:
+else:
             print("Higher")
 
     guess = int(input("Please guess again: "))
