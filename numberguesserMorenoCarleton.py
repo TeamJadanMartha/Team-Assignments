0cd92028a4089e4426c718f0c727a89c75aa7609
jadancarleton <jcarleton1@my.nl.edu>
2025-02-21 19:24:42 -0600

    Add files via upload

---
+++ numberguesserMorenoCarleton.py
@@ -0,0 +1,26 @@
+#Martha Moreno Gonzalez and Jadan Carleton
+#Friday, February 21st, 2025
+
+#This program allows the user to try and guess a number between 0 and 10
+#and gives hints if their guess should be higher or lower.
+
+import random
+
+answer = random.randrange(1,11)
+
+guess = int(input("Please guess a number between 1 and 10: "))
+
+for i in range(100):
+        
+    if guess == answer:
+            print("Well done, you guessed it!")
+            break
+    elif guess > answer:
+            print("Lower")
+    else:
+            print("Higher")
+
+    guess = int(input("Please guess again: "))
+
+
+print("Thank you for playing!")
